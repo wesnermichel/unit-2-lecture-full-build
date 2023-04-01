f13b09337a3d35f40749af2279f6d47846fb708a
Joe Keohan <jkeohan@gmail.com>
2023-04-01 10:53:51 -0400

    updates

--- solution_code/db/connection.js
+++ solution_code/db/connection.js
@@ -9,23 +9,17 @@ const DATABASE_URL =
 // mongoose connect to url
 mongoose.connect(DATABASE_URL)
 
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to mongoDB")
+})
+
+mongoose.connection.on("error", (error) => {
+    console.log("mongoDB Connection Error:", error)
+})
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected")
+})
+
 // export mongoose 
 module.exports = { mongoose }
-
-// const mongoose = require("mongoose")
-
-// const connectionString = process.env.DATABASE_URL
-
-// mongoose.connect(connectionString)
-
-// mongoose.connection.on("connected", () => {
-//     console.log("Mongoose connected to mongoDB")
-// })
-
-// mongoose.connection.on("error", (error) => {
-//     console.log("mongoDB Connection Error:", error)
-// })
-
-// mongoose.connection.on("disconnected", () => {
-//     console.log("mongoDB disconnected")
-// })
